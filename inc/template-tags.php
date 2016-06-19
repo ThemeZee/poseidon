725f18028b6dd469ca75265a963df54aaec59a88
Thomas W <netzberufler@googlemail.com>
2016-06-19 17:13:55 +0200

    improved accessibility of post navigation and pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -307,7 +307,10 @@ function poseidon_post_navigation() {
 
 	if ( true === $theme_options['post_navigation'] ) {
 
-		the_post_navigation( array( 'prev_text' => '&laquo; %title', 'next_text' => '%title &raquo;' ) );
+		the_post_navigation( array(
+			'prev_text' => '<span class="screen-reader-text">' . esc_html_x( 'Previous Post:', 'post navigation', 'poseidon' ) . '</span>%title',
+			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Post:', 'post navigation', 'poseidon' ) . '</span>%title',
+		) );
 
 	}
 
@@ -367,8 +370,8 @@ function poseidon_pagination() {
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var( 'paged' ) ),
 		'total' => $wp_query->max_num_pages,
-		'next_text' => '&raquo;',
-		'prev_text' => '&laquo',
+		'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'poseidon' ) . '</span>&raquo;',
+		'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'poseidon' ) . '</span>',
 		'add_args' => false,
 	) );
 
