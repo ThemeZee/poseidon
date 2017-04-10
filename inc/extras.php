e1419d3fa50ca48eef9c2174ee08b2e4bfa71cb7
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-04-10 09:12:41 +0200

    fixed horizontal scrolling issue when elements are hidden on mobile devices

--- inc/extras.php
+++ inc/extras.php
@@ -99,7 +99,7 @@ function poseidon_hide_elements() {
 
 	// Create CSS.
 	$classes = implode( ', ', $elements );
-	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); }';
+	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); width: 1px; height: 1px; overflow: hidden; }';
 
 	// Add Custom CSS.
 	wp_add_inline_style( 'poseidon-stylesheet', $custom_css );
