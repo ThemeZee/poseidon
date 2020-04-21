d39c010cd1eb39daebd46d07866155504a1218d6
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-04-21 09:43:55 +0200

    added own section file for website settings in Customizer

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -24,6 +24,7 @@ require( get_template_directory() . '/inc/customizer/sections/customizer-post.ph
 require( get_template_directory() . '/inc/customizer/sections/customizer-magazine.php' );
 require( get_template_directory() . '/inc/customizer/sections/customizer-slider.php' );
 require( get_template_directory() . '/inc/customizer/sections/customizer-info.php' );
+require( get_template_directory() . '/inc/customizer/sections/customizer-website.php' );
 
 /**
  * Registers Theme Options panel and sets up some WordPress core settings
@@ -43,98 +44,8 @@ function poseidon_customize_register_options( $wp_customize ) {
 	// Change default background section.
 	$wp_customize->get_control( 'background_color' )->section = 'background_image';
 	$wp_customize->get_section( 'background_image' )->title   = esc_html__( 'Background', 'poseidon' );
-
-	// Add postMessage support for site title and description.
-	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
-
-	// Add selective refresh for site title and description.
-	$wp_customize->selective_refresh->add_partial( 'blogname', array(
-		'selector'        => '.site-title a',
-		'render_callback' => 'poseidon_customize_partial_blogname',
-	) );
-	$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-		'selector'        => '.site-description',
-		'render_callback' => 'poseidon_customize_partial_blogdescription',
-	) );
-
-	// Add Display Site Title Setting.
-	$wp_customize->add_setting( 'poseidon_theme_options[site_title]', array(
-		'default'           => true,
-		'type'              => 'option',
-		'transport'         => 'postMessage',
-		'sanitize_callback' => 'poseidon_sanitize_checkbox',
-	) );
-	$wp_customize->add_control( 'poseidon_theme_options[site_title]', array(
-		'label'    => esc_html__( 'Display Site Title', 'poseidon' ),
-		'section'  => 'title_tagline',
-		'settings' => 'poseidon_theme_options[site_title]',
-		'type'     => 'checkbox',
-		'priority' => 10,
-	) );
-
-	// Add Display Tagline Setting.
-	$wp_customize->add_setting( 'poseidon_theme_options[site_description]', array(
-		'default'           => false,
-		'type'              => 'option',
-		'transport'         => 'postMessage',
-		'sanitize_callback' => 'poseidon_sanitize_checkbox',
-	) );
-	$wp_customize->add_control( 'poseidon_theme_options[site_description]', array(
-		'label'    => esc_html__( 'Display Tagline', 'poseidon' ),
-		'section'  => 'title_tagline',
-		'settings' => 'poseidon_theme_options[site_description]',
-		'type'     => 'checkbox',
-		'priority' => 11,
-	) );
-
-	// Add Header Image Link.
-	$wp_customize->add_setting( 'poseidon_theme_options[custom_header_link]', array(
-		'default'           => '',
-		'type'              => 'option',
-		'transport'         => 'refresh',
-		'sanitize_callback' => 'esc_url',
-	) );
-	$wp_customize->add_control( 'poseidon_theme_options[custom_header_link]', array(
-		'label'    => esc_html__( 'Header Image Link', 'poseidon' ),
-		'section'  => 'header_image',
-		'settings' => 'poseidon_theme_options[custom_header_link]',
-		'type'     => 'url',
-		'priority' => 10,
-	) );
-	// Add Custom Header Hide Checkbox.
-	$wp_customize->add_setting( 'poseidon_theme_options[custom_header_hide]', array(
-		'default'           => false,
-		'type'              => 'option',
-		'transport'         => 'refresh',
-		'sanitize_callback' => 'poseidon_sanitize_checkbox',
-	) );
-	$wp_customize->add_control( 'poseidon_theme_options[custom_header_hide]', array(
-		'label'    => esc_html__( 'Hide header image on front page', 'poseidon' ),
-		'section'  => 'header_image',
-		'settings' => 'poseidon_theme_options[custom_header_hide]',
-		'type'     => 'checkbox',
-		'priority' => 15,
-	) );
-
-} // poseidon_customize_register_options()
+}
 add_action( 'customize_register', 'poseidon_customize_register_options' );
-
-
-/**
- * Render the site title for the selective refresh partial.
- */
-function poseidon_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-
-/**
- * Render the site tagline for the selective refresh partial.
- */
-function poseidon_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
 
 
 /**
---
+++ inc/customizer/sections/customizer-website.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Site Identity Settings
+ *
+ * Register settings to hide site title and tagline in Site Identity section
+ *
+ * @package Poseidon
+ */
+
+/**
+ * Adds Site Title settings to the Customizer
+ *
+ * @param object $wp_customize / Customizer Object.
+ */
+function poseidon_customize_register_website_settings( $wp_customize ) {
+
+	// Add postMessage support for site title and description.
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+
+	// Add selective refresh for site title and description.
+	$wp_customize->selective_refresh->add_partial( 'blogname', array(
+		'selector'        => '.site-title a',
+		'render_callback' => 'poseidon_customize_partial_blogname',
+	) );
+	$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+		'selector'        => '.site-description',
+		'render_callback' => 'poseidon_customize_partial_blogdescription',
+	) );
+
+	// Add Display Site Title Setting.
+	$wp_customize->add_setting( 'poseidon_theme_options[site_title]', array(
+		'default'           => true,
+		'type'              => 'option',
+		'transport'         => 'postMessage',
+		'sanitize_callback' => 'poseidon_sanitize_checkbox',
+	) );
+	$wp_customize->add_control( 'poseidon_theme_options[site_title]', array(
+		'label'    => esc_html__( 'Display Site Title', 'poseidon' ),
+		'section'  => 'title_tagline',
+		'settings' => 'poseidon_theme_options[site_title]',
+		'type'     => 'checkbox',
+		'priority' => 10,
+	) );
+
+	// Add Display Tagline Setting.
+	$wp_customize->add_setting( 'poseidon_theme_options[site_description]', array(
+		'default'           => false,
+		'type'              => 'option',
+		'transport'         => 'postMessage',
+		'sanitize_callback' => 'poseidon_sanitize_checkbox',
+	) );
+	$wp_customize->add_control( 'poseidon_theme_options[site_description]', array(
+		'label'    => esc_html__( 'Display Tagline', 'poseidon' ),
+		'section'  => 'title_tagline',
+		'settings' => 'poseidon_theme_options[site_description]',
+		'type'     => 'checkbox',
+		'priority' => 11,
+	) );
+
+	// Add Header Image Link.
+	$wp_customize->add_setting( 'poseidon_theme_options[custom_header_link]', array(
+		'default'           => '',
+		'type'              => 'option',
+		'transport'         => 'refresh',
+		'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'poseidon_theme_options[custom_header_link]', array(
+		'label'    => esc_html__( 'Header Image Link', 'poseidon' ),
+		'section'  => 'header_image',
+		'settings' => 'poseidon_theme_options[custom_header_link]',
+		'type'     => 'url',
+		'priority' => 10,
+	) );
+	// Add Custom Header Hide Checkbox.
+	$wp_customize->add_setting( 'poseidon_theme_options[custom_header_hide]', array(
+		'default'           => false,
+		'type'              => 'option',
+		'transport'         => 'refresh',
+		'sanitize_callback' => 'poseidon_sanitize_checkbox',
+	) );
+	$wp_customize->add_control( 'poseidon_theme_options[custom_header_hide]', array(
+		'label'    => esc_html__( 'Hide header image on front page', 'poseidon' ),
+		'section'  => 'header_image',
+		'settings' => 'poseidon_theme_options[custom_header_hide]',
+		'type'     => 'checkbox',
+		'priority' => 15,
+	) );
+}
+add_action( 'customize_register', 'poseidon_customize_register_website_settings' );
+
+
+/**
+ * Render the site title for the selective refresh partial.
+ */
+function poseidon_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ */
+function poseidon_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
