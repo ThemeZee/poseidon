ba6e9c6013c231c10f89af3af31e4d1b6120c69f
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-03-06 17:21:20 +0100

    changed implementation of Gutenberg features

--- functions.php
+++ functions.php
@@ -78,35 +78,6 @@ if ( ! function_exists( 'poseidon_setup' ) ) :
 		// Add Theme Support for Selective Refresh in Customizer.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-		// Add custom color palette for Gutenberg.
-		add_theme_support( 'editor-color-palette', array(
-			array(
-				'name'  => esc_html_x( 'Primary', 'Gutenberg Color Palette', 'poseidon' ),
-				'slug'  => 'primary',
-				'color' => apply_filters( 'poseidon_primary_color', '#22aadd' ),
-			),
-			array(
-				'name'  => esc_html_x( 'White', 'Gutenberg Color Palette', 'poseidon' ),
-				'slug'  => 'white',
-				'color' => '#ffffff',
-			),
-			array(
-				'name'  => esc_html_x( 'Light Gray', 'Gutenberg Color Palette', 'poseidon' ),
-				'slug'  => 'light-gray',
-				'color' => '#f0f0f0',
-			),
-			array(
-				'name'  => esc_html_x( 'Dark Gray', 'Gutenberg Color Palette', 'poseidon' ),
-				'slug'  => 'dark-gray',
-				'color' => '#777777',
-			),
-			array(
-				'name'  => esc_html_x( 'Black', 'Gutenberg Color Palette', 'poseidon' ),
-				'slug'  => 'black',
-				'color' => '#404040',
-			),
-		) );
-
 		// Add support for responsive embed blocks.
 		add_theme_support( 'responsive-embeds' );
 	}
@@ -242,15 +213,6 @@ function poseidon_get_fonts_url() {
 
 
 /**
- * Enqueue editor styles for the new Gutenberg Editor.
- */
-function poseidon_block_editor_assets() {
-	wp_enqueue_style( 'poseidon-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20191118', 'all' );
-}
-add_action( 'enqueue_block_editor_assets', 'poseidon_block_editor_assets' );
-
-
-/**
  * Add custom sizes for featured images
  */
 function poseidon_add_image_sizes() {
@@ -300,6 +262,9 @@ require get_template_directory() . '/inc/extras.php';
 
 // Include Template Functions.
 require get_template_directory() . '/inc/template-tags.php';
+
+// Include Gutenberg Features.
+require get_template_directory() . '/inc/gutenberg.php';
 
 // Include support functions for Theme Addons.
 require get_template_directory() . '/inc/addons.php';
---
+++ inc/gutenberg.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Add theme support for the Gutenberg Editor
+ *
+ * @package Poseidon
+ */
+
+
+/**
+ * Registers support for various Gutenberg features.
+ *
+ * @return void
+ */
+function poseidon_gutenberg_support() {
+
+	// Add theme support for wide and full images.
+	#add_theme_support( 'align-wide' );
+
+	// Define block color palette.
+	$color_palette = apply_filters( 'poseidon_color_palette', array(
+		'primary_color'    => '#cc5555',
+		'secondary_color'  => '#5d7b94',
+		'tertiary_color'   => '#90aec7',
+		'accent_color'     => '#60945d',
+		'highlight_color'  => '#915d94',
+		'light_gray_color' => '#f0f0f0',
+		'gray_color'       => '#999999',
+		'dark_gray_color'  => '#303030',
+	) );
+
+	// Add theme support for block color palette.
+	add_theme_support( 'editor-color-palette', apply_filters( 'poseidon_editor_color_palette_args', array(
+		array(
+			'name'  => esc_html_x( 'Primary', 'block color', 'poseidon' ),
+			'slug'  => 'primary',
+			'color' => esc_html( $color_palette['primary_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Secondary', 'block color', 'poseidon' ),
+			'slug'  => 'secondary',
+			'color' => esc_html( $color_palette['secondary_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Tertiary', 'block color', 'poseidon' ),
+			'slug'  => 'tertiary',
+			'color' => esc_html( $color_palette['tertiary_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Accent', 'block color', 'poseidon' ),
+			'slug'  => 'accent',
+			'color' => esc_html( $color_palette['accent_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Highlight', 'block color', 'poseidon' ),
+			'slug'  => 'highlight',
+			'color' => esc_html( $color_palette['highlight_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'White', 'block color', 'poseidon' ),
+			'slug'  => 'white',
+			'color' => '#ffffff',
+		),
+		array(
+			'name'  => esc_html_x( 'Light Gray', 'block color', 'poseidon' ),
+			'slug'  => 'light-gray',
+			'color' => esc_html( $color_palette['light_gray_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Gray', 'block color', 'poseidon' ),
+			'slug'  => 'gray',
+			'color' => esc_html( $color_palette['gray_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Dark Gray', 'block color', 'poseidon' ),
+			'slug'  => 'dark-gray',
+			'color' => esc_html( $color_palette['dark_gray_color'] ),
+		),
+		array(
+			'name'  => esc_html_x( 'Black', 'block color', 'poseidon' ),
+			'slug'  => 'black',
+			'color' => '#000000',
+		),
+	) ) );
+}
+add_action( 'after_setup_theme', 'poseidon_gutenberg_support' );
+
+
+/**
+ * Enqueue block styles and scripts for Gutenberg Editor.
+ */
+function poseidon_block_editor_assets() {
+
+	// Enqueue Editor Styling.
+	wp_enqueue_style( 'poseidon-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20210306', 'all' );
+
+	// Enqueue Page Template Switcher Editor plugin.
+	#wp_enqueue_script( 'poseidon-page-template-switcher', get_theme_file_uri( '/assets/js/page-template-switcher.js' ), array( 'wp-blocks', 'wp-element', 'wp-edit-post' ), '20210306' );
+}
+add_action( 'enqueue_block_editor_assets', 'poseidon_block_editor_assets' );
+
+
+/**
+ * Remove inline styling in Gutenberg.
+ *
+ * @return array $editor_settings
+ */
+function poseidon_block_editor_settings( $editor_settings ) {
+	// Remove editor styling.
+	if ( ! current_theme_supports( 'editor-styles' ) ) {
+		$editor_settings['styles'] = '';
+	}
+
+	return $editor_settings;
+}
+#add_filter( 'block_editor_settings', 'poseidon_block_editor_settings', 11 );
+
+
+/**
+ * Add body classes in Gutenberg Editor.
+ */
+function poseidon_block_editor_body_classes( $classes ) {
+	global $post;
+	$current_screen = get_current_screen();
+
+	// Return early if we are not in the Gutenberg Editor.
+	if ( ! ( method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) ) {
+		return $classes;
+	}
+
+	// Fullwidth Page Template?
+	if ( 'templates/template-fullwidth.php' === get_page_template_slug( $post->ID ) ) {
+		$classes .= ' poseidon-fullwidth-page-layout ';
+	}
+
+	// No Title Page Template?
+	if ( 'templates/template-no-title.php' === get_page_template_slug( $post->ID ) or
+		'templates/template-sidebar-left-no-title.php' === get_page_template_slug( $post->ID ) or
+		'templates/template-sidebar-right-no-title.php' === get_page_template_slug( $post->ID ) ) {
+		$classes .= ' poseidon-page-title-hidden ';
+	}
+
+	// Full-width / No Title Page Template?
+	if ( 'templates/template-fullwidth-no-title.php' === get_page_template_slug( $post->ID ) ) {
+		$classes .= ' poseidon-fullwidth-page-layout poseidon-page-title-hidden ';
+	}
+
+	return $classes;
+}
+#add_filter( 'admin_body_class', 'poseidon_block_editor_body_classes' );
