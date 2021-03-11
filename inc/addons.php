eac3e68bce1aa6ab8c73cbb8f4718c47d5f83ce3
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-03-11 10:34:33 +0100

    added external plugin styling to main stylesheet

--- inc/addons.php
+++ inc/addons.php
@@ -42,36 +42,6 @@ add_action( 'after_setup_theme', 'poseidon_theme_addons_setup' );
 
 
 /**
- * Load custom stylesheets for theme addons
- */
-function poseidon_theme_addons_scripts() {
-
-	// Load widget bundle styles if widgets are active.
-	if ( is_active_widget( 'TZWB_Facebook_Likebox_Widget', false, 'tzwb-facebook-likebox' )
-		or is_active_widget( 'TZWB_Recent_Comments_Widget', false, 'tzwb-recent-comments' )
-		or is_active_widget( 'TZWB_Recent_Posts_Widget', false, 'tzwb-recent-posts' )
-		or is_active_widget( 'TZWB_Social_Icons_Widget', false, 'tzwb-social-icons' )
-		or is_active_widget( 'TZWB_Tabbed_Content_Widget', false, 'tzwb-tabbed-content' )
-	) {
-
-		// Enqueue Widget Bundle stylesheet.
-		wp_enqueue_style( 'themezee-widget-bundle', get_template_directory_uri() . '/assets/css/themezee-widget-bundle.css', array(), '20160421' );
-
-	}
-
-	// Load Related Posts stylesheet only on single posts.
-	if ( is_singular( 'post' ) ) {
-
-		// Enqueue Related Post stylesheet.
-		wp_enqueue_style( 'themezee-related-posts', get_template_directory_uri() . '/assets/css/themezee-related-posts.css', array(), '20160421' );
-
-	}
-
-}
-add_action( 'wp_enqueue_scripts', 'poseidon_theme_addons_scripts' );
-
-
-/**
  * Custom render function for Infinite Scroll.
  */
 function poseidon_infinite_scroll_render() {
