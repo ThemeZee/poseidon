77640cc3f7df667ded09f1041a394993cecf8d28
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-04 12:35:56 +0200

    removed Genericons icon font

--- functions.php
+++ functions.php
@@ -141,9 +141,6 @@ function poseidon_scripts() {
 	// Register and Enqueue Stylesheet.
 	wp_enqueue_style( 'poseidon-stylesheet', get_stylesheet_uri(), array(), $theme_version );
 
-	// Register Genericons.
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/assets/genericons/genericons.css', array(), '3.4.1' );
-
 	// Register and Enqueue HTML5shiv to support HTML5 elements in older IE versions.
 	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.min.js', array(), '3.7.3' );
 	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
