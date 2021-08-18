fcfca597793b77760b5fd6a20a3a8c594f7cb6f1
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-08-18 17:20:16 +0200

    updated block styling

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -100,22 +100,6 @@ add_action( 'enqueue_block_editor_assets', 'poseidon_block_editor_assets' );
 
 
 /**
- * Remove inline styling in Gutenberg.
- *
- * @return array $editor_settings
- */
-function poseidon_block_editor_settings( $editor_settings ) {
-	// Remove editor styling.
-	if ( ! current_theme_supports( 'editor-styles' ) ) {
-		$editor_settings['styles'] = '';
-	}
-
-	return $editor_settings;
-}
-#add_filter( 'block_editor_settings', 'poseidon_block_editor_settings', 11 );
-
-
-/**
  * Add body classes in Gutenberg Editor.
  */
 function poseidon_block_editor_body_classes( $classes ) {
