cf33dd09e28119ac9a794c3946cb046e3d724e80
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-11-05 10:02:11 +0100

    replaced jquery navigation with vanilla js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,118 +5,97 @@
  * @package Poseidon
  */
 
-(function( $ ) {
+ (function() {
+
+	// Create dropdown toggle button.
+	function createDropdownToggle() {
+		var dropdownToggle = document.createElement( 'button' );
+
+		// Add classes and aria attributes.
+		dropdownToggle.classList.add( 'dropdown-toggle' );
+		dropdownToggle.setAttribute( 'aria-expanded', 'false' );
+
+		// Add icon to dropdown toggle.
+		var icon = new DOMParser().parseFromString( poseidonScreenReaderText.icon, 'text/html' ).body.firstElementChild;
+		dropdownToggle.appendChild( icon);
+
+		// Add screenreader text.
+		var screenReaderText = document.createElement( 'span' );
+		screenReaderText.classList.add( 'screen-reader-text' );
+		screenReaderText.textContent = poseidonScreenReaderText.expand;
+		dropdownToggle.appendChild( screenReaderText );
+
+		return dropdownToggle.cloneNode(true);
+	}
 
 	function initNavigation( containerClass, naviClass ) {
-		var container  = $( containerClass );
-		var navigation = $( naviClass );
+		var container  = document.querySelector( containerClass );
+		var navigation = document.querySelector( naviClass );
 
 		// Return early if navigation is missing.
-		if ( ! navigation.length ) {
+		if ( navigation === null ) {
 			return;
 		}
 
 		// Enable menuToggle.
 		(function() {
-			var menuToggle = container.find( '.menu-toggle' );
+			var menuToggle = container.querySelector( '.menu-toggle' );
 
 			// Return early if menuToggle is missing.
-			if ( ! menuToggle.length ) {
+			if ( menuToggle === null ) {
 				return;
 			}
 
 			// Add an initial value for the attribute.
-			menuToggle.attr( 'aria-expanded', 'false' );
+			menuToggle.setAttribute( 'aria-expanded', 'false' );
 
-			menuToggle.on( 'click.poseidon_', function() {
-				navigation.toggleClass( 'toggled-on' );
-
-				$( this ).attr( 'aria-expanded', navigation.hasClass( 'toggled-on' ) );
+			// Menu Toggle click event.
+			menuToggle.addEventListener( 'click', function() {
+				navigation.classList.toggle( 'toggled-on' );
+				menuToggle.setAttribute( 'aria-expanded', navigation.classList.contains( 'toggled-on' ) );
 			});
 		})();
 
 		// Enable dropdownToggles that displays child menu items.
 		(function() {
 
-			var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false } )
-				.append( poseidonScreenReaderText.icon )
-				.append( $( '<span />', { 'class': 'screen-reader-text', text: poseidonScreenReaderText.expand } ) );
-
-			navigation.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
+			// Insert dropdown toggles in navigation menu.
+			navigation.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' ).forEach( function( menuItem ) {
+				menuItem.after( createDropdownToggle() );
+			});
 
 			// Set the active submenu dropdown toggle button initial state.
-			navigation.find( '.current-menu-ancestor > button' )
-				.addClass( 'toggled-on' )
-				.attr( 'aria-expanded', 'true' )
-				.find( '.screen-reader-text' )
-				.text( poseidonScreenReaderText.collapse );
+			navigation.querySelectorAll( '.current-menu-ancestor > button' ).forEach( function( activeToggle ) {
+				activeToggle.classList.add( 'toggled-on' );
+				activeToggle.setAttribute( 'aria-expanded', 'true' );
+				activeToggle.querySelector( '.screen-reader-text' ).textContent = poseidonScreenReaderText.collapse;
+			});
 
 			// Set the active submenu initial state.
-			navigation.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
-
-			navigation.find( '.dropdown-toggle' ).click( function( e ) {
-				var _this = $( this ),
-					screenReaderSpan = _this.find( '.screen-reader-text' );
-
-				e.preventDefault();
-				_this.toggleClass( 'toggled-on' );
-				_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
-
-				_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
-
-				screenReaderSpan.text( screenReaderSpan.text() === poseidonScreenReaderText.expand ? poseidonScreenReaderText.collapse : poseidonScreenReaderText.expand );
-			} );
-		})();
-
-		// Fix sub-menus for touch devices and better focus for hidden submenu items for accessibility.
-		(function() {
-			var menuList   = navigation.children( 'ul.menu' );
-
-			if ( ! menuList.length || ! menuList.children().length ) {
-				return;
-			}
-
-			// Toggle `focus` class to allow submenu access on tablets.
-			function toggleFocusClassTouchScreen() {
-				if ( 'none' === $( '.menu-toggle' ).css( 'display' ) ) {
-
-					$( document.body ).on( 'touchstart.poseidon_', function( e ) {
-						if ( ! $( e.target ).closest( naviClass + ' li' ).length ) {
-							$( naviClass + ' li' ).removeClass( 'focus' );
-						}
-					});
-
-					menuList.find( '.menu-item-has-children > a, .page_item_has_children > a' )
-						.on( 'touchstart.poseidon_', function( e ) {
-							var el = $( this ).parent( 'li' );
-
-							if ( ! el.hasClass( 'focus' ) ) {
-								e.preventDefault();
-								el.toggleClass( 'focus' );
-								el.siblings( '.focus' ).removeClass( 'focus' );
-							}
-						});
-
-				} else {
-					menuList.find( '.menu-item-has-children > a, .page_item_has_children > a' ).unbind( 'touchstart.poseidon_' );
-				}
-			}
-
-			if ( 'ontouchstart' in window ) {
-				$( window ).on( 'resize.poseidon_', toggleFocusClassTouchScreen );
-				toggleFocusClassTouchScreen();
-			}
-
-			menuList.find( 'a' ).on( 'focus.poseidon_ blur.poseidon_', function() {
-				$( this ).parents( '.menu-item, .page_item' ).toggleClass( 'focus' );
+			navigation.querySelectorAll( '.current-menu-ancestor > .sub-menu' ).forEach( function( activeSubmenu ) {
+				activeSubmenu.classList.add( 'toggled-on' );
+			});
+	
+			// Dropdown Toggles click events.
+			navigation.querySelectorAll( '.dropdown-toggle' ).forEach( function( dropdownItem ) {
+				dropdownItem.addEventListener( 'click', function() {
+					dropdownItem.classList.toggle( 'toggled-on' );
+					dropdownItem.setAttribute( 'aria-expanded', dropdownItem.classList.contains( 'toggled-on' ) );
+					dropdownItem.querySelector( '.screen-reader-text' ).textContent = dropdownItem.classList.contains( 'toggled-on' ) ? poseidonScreenReaderText.collapse : poseidonScreenReaderText.expand;
+					dropdownItem.nextElementSibling.classList.toggle( 'toggled-on' );
+				});
 			});
 		})();
 	}
 
-	// Init Main Navigation.
-	initNavigation( '.header-main', '.main-navigation' );
+	document.addEventListener( 'DOMContentLoaded', function() {
 
-	// Init Top Navigation.
-	initNavigation( '.header-bar', '.top-navigation' );
+		// Init Main Navigation.
+		initNavigation( '.header-main', '.main-navigation' );
 
-})( jQuery );
+		// Init Top Navigation.
+		initNavigation( '.header-bar', '.top-navigation' );
+
+	} );
+
+}() );
