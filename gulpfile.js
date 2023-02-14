c0442e63bba943cb72fdfa6cd2aec459e8e6ec7e
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2023-02-14 11:53:06 +0100

    cleaned up node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-// Include gulp
+// Include Gulp.
 var gulp = require('gulp');
 
-// Include plugins
-var autoprefixer = require( 'autoprefixer' );
-var rename       = require( 'gulp-rename' );
-var replace      = require( 'gulp-replace' );
-var uglify       = require( 'gulp-uglify' );
-var sass         = require('gulp-sass')(require('sass'));
-var postcss      = require( 'gulp-postcss' );
-var sorting      = require( 'postcss-sorting' );
-var stylelint       = require( 'stylelint' );
+// Include plugins.
+var rename    = require( 'gulp-rename' );
+var uglify    = require( 'gulp-uglify' );
+var sass      = require('gulp-sass')(require('sass'));
+var postcss   = require( 'gulp-postcss' );
+var sorting   = require( 'postcss-sorting' );
+var stylelint = require( 'stylelint' );
 
-// Minify JS
+// Minify JS.
 gulp.task( 'minifyjs', async function() {
 	return gulp.src( ['assets/js/navigation.js', 'assets/js/customize-preview.js', 'assets/js/customizer-controls.js'] )
 		.pipe( uglify() )
@@ -21,46 +19,27 @@ gulp.task( 'minifyjs', async function() {
 		.pipe( gulp.dest('assets/js') );
 });
 
-// Clean up CSS
-gulp.task( 'cleancss', async function() {
-	return gulp.src( ['style.css', 'assets/css/*.css'], { base: './' } )
-		.pipe( postcss( [ autoprefixer() ] ) )
-		.pipe( postcss( [ sorting( { 'preserve-empty-lines-between-children-rules': true } ) ] ) )
-		.pipe( gulp.dest( './' ) );
-});
-
-// Editor Styles Sass Bundler
+// Editor Styles Sass Bundler.
 gulp.task( 'editor', async function() {
     return gulp.src( 'sass/editor-styles.scss' )
         .pipe( sass( { outputStyle: 'expanded' } ).on( 'error', sass.logError ) )
 		.pipe( rename( 'editor-styles.css' ) )
 		.pipe( postcss( [ sorting() ] ) )
-		.pipe( replace( '  ', '	' ) )
-		.pipe( replace( '}\n	', '}\n\n	' ) )
-		.pipe( replace( '}\n\n	}', '}\n	}' ) )
-		.pipe( replace( '*/\n/*', '*/\n\n/*' ) )
-		.pipe( replace( ';\n	/*', '; /*' ) )
 		.pipe( postcss( [ stylelint( { 'fix': true } ) ] ) )
         .pipe( gulp.dest( 'assets/css' ) )
 });
 
-// Sass Bundler
+// Sass Bundler.
 gulp.task( 'sass', async function() {
     return gulp.src( 'sass/style.scss' )
         .pipe( sass( { outputStyle: 'expanded' } ).on( 'error', sass.logError ) )
 		.pipe( rename( 'style.css' ) )
 		.pipe( postcss( [ sorting() ] ) )
-		.pipe( replace( '  ', '	' ) )
-		.pipe( replace( '}\n	', '}\n\n	' ) )
-		.pipe( replace( '}\n\n	}', '}\n	}' ) )
-		.pipe( replace( '*/\n/*', '*/\n\n/*' ) )
-		.pipe( replace( ';\n	/*', '; /*' ) )
-		.pipe( replace( '	}\n}', '	}\n}\n' ) )
 		.pipe( postcss( [ stylelint( { 'fix': true } ) ] ) )
         .pipe( gulp.dest( './' ) )
 });
 
-// Sass Watch
+// Sass Watch.
 gulp.task('sass:watch', async function () {
 	gulp.watch( 'sass/**/*.scss', gulp.series('sass', 'editor'));
 });
